43a339a878eccf178a1af28d07afe3477460ddf0
barbizu <belenarbizu98@gmail.com>
2026-03-23 17:47:31 +0100

    d1ex03

---
+++ Starting/ex03/capital_city.py
@@ -0,0 +1,28 @@
+import sys
+
+def main():
+    states = {
+        "Oregon" : "OR",
+        "Alabama" : "AL",
+        "New Jersey": "NJ",
+        "Colorado" : "CO"
+    }
+
+    capital_cities = {
+        "OR": "Salem",
+        "AL": "Montgomery",
+        "NJ": "Trenton",
+        "CO": "Denver"
+    }
+
+    args = sys.argv
+    if len(args) == 2:
+        state = args[1]
+        if state in states:
+            name = states[state]
+            capital = capital_cities[name]
+            print(capital)
+
+
+if __name__ == '__main__':
+    main()
