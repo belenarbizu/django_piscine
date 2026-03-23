e1354568081bab6030b7663a243100dd3f3e4619
barbizu <belenarbizu98@gmail.com>
2026-03-23 17:08:11 +0100

    d1ex00

---
+++ Starting/ex00/var.py
@@ -0,0 +1,9 @@
+
+def myvar():
+    var_list = [42, "42", "quarante-deux", 42.0, True, [42], {42: 42}, (42,), set()]
+
+    for var in var_list:
+        print(f"{var} has a type {type(var)}")
+
+if __name__ == "__main__":
+    myvar()
