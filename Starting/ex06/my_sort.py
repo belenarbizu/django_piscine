9dfae488d71bdc5b86088ef20332ef99b83c216b
barbizu <belenarbizu98@gmail.com>
2026-03-23 19:55:09 +0100

    d1ex06

---
+++ Starting/ex06/my_sort.py
@@ -0,0 +1,37 @@
+def sort_by_year(d):
+    """Display the names of the musicians sorted by year in ascending order and alphabetic order. One per line without showing the year."""
+    for key, value in d.items():
+        d[key] = int(value)
+    sorted_d = sorted(d.items(), key=lambda x: (x[1], x[0]))
+    for item in sorted_d:
+        print(item[0])
+
+
+def main():
+    d = {
+        'Hendrix' : '1942',
+        'Allman' : '1946',
+        'King' : '1925',
+        'Clapton' : '1945',
+        'Johnson' : '1911',
+        'Berry' : '1926',
+        'Vaughan' : '1954',
+        'Cooder' : '1947',
+        'Page' : '1944',
+        'Richards' : '1943',
+        'Hammett' : '1962',
+        'Cobain' : '1967',
+        'Garcia' : '1942',
+        'Beck' : '1944',
+        'Santana' : '1947',
+        'Ramone' : '1948',
+        'White' : '1975',
+        'Frusciante': '1970',
+        'Thompson' : '1949',
+        'Burton' : '1939',
+    }
+    sort_by_year(d)
+
+
+if __name__ == '__main__':
+    main()
