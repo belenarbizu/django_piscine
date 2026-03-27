6a27f983cfe2ff1010e8803089492a128464fa8b
barbizu <belenarbizu98@gmail.com>
2026-03-27 16:55:30 +0100

    d3ex04

---
+++ Lib/ex04/my_script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+rm -rf django_venv
+
+if python3 -m venv django_venv; then
+    if [ -f "requirement.txt" ]; then
+        ./django_venv/bin/python3 -m pip install -r requirement.txt
+    else
+        echo "requirement.txt not found."
+    fi
+    # Activate the environment
+    . django_venv/bin/activate
+else
+    echo "Error: Failed to create the virtual environment."
+    return 1 2>/dev/null || exit 1
+fi
