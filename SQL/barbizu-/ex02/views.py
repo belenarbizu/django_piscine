d51cdf69a20e3a5c86aa9b7f2f42ce664a86fd3e
barbizu <belenarbizu98@gmail.com>
2026-04-01 16:50:43 +0200

    d5ex02

--- SQL/barbizu-/ex02/views.py
+++ SQL/barbizu-/ex02/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.conf import settings
 import psycopg2
+from psycopg2.extras import DictCursor
 
 # Create your views here.
 def init(request):
@@ -52,7 +53,7 @@ def populate(request):
                         ('Revenge of the Sith', 3, 'George Lucas', 'Rick McCallum', '2005-05-19'),
                         ('A New Hope', 4, 'George Lucas', 'Gary Kurtz, Rick McCallum', '1977-05-25'),
                         ('The Empire Strikes Back', 5, 'Irvin Kershner', 'Gary Kurtz, Rick McCallum', '1980-05-17'),
-                        ('Return of the Jedi', 6, 'Richard Marquand', 'Howard G. Kazanjian, George Lucas, Rick McCallum', '1983-05-25')
+                        ('Return of the Jedi', 6, 'Richard Marquand', 'Howard G. Kazanjian, George Lucas, Rick McCallum', '1983-05-25'),
                         ('The Force Awakens', 7, 'J. J. Abrams', 'Kathleen Kennedy, J. J. Abrams, Bryan Burk', '2015-12-11')
                     """)
                     conn.commit() # Without commit, it won't save the modifications
@@ -76,11 +77,11 @@ def display(request):
             port=db_config['PORT']
         )
         with conn:
-            with conn.cursor() as cur:
+            with conn.cursor(cursor_factory=DictCursor) as cur:
                 cur.execute("SELECT * FROM ex02_movies")
                 movies = cur.fetchall()
                 if not movies:
                     return HttpResponse("No data available.")
-                return render(request, 'ex02_display.html', {'movies': movies})
+                return render(request, 'ex02/ex02_display.html', {'movies': movies})
     except Exception as e:
         return HttpResponse(f"Error: {e}")
